c43070798fbf7924fa105eb32220f5c90c8be971
Alexandr Tumanov <shurinskiy@users.noreply.github.com>
2020-10-11 15:19:19 +0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 'use strict';
+'use strict';
 var gulp = require('gulp');
 var sprite = require('./');
 
 gulp.task('default', function () {
-    return gulp.src(['./fixture/*.svg'])
-        .pipe(sprite({
-            svg: 'my-sprites.svg',
-            json: 'my-sprites.json'
-        }))
-        .pipe(gulp.dest('./dest'));
+	return gulp.src(['./fixture/*.svg'])
+	.pipe(sprite({
+		name: 'sprite.svg',
+		monochrome: {
+			darkcyan: '#008b8b',
+			burlywood: '#deb887',
+			dimgrey: '#696969'
+		}
+	}))
+	.pipe(gulp.dest('./dest'));
 });
