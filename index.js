be0d27402ac8470e630cb6cb5dcbc680867a6a40
Alexandr Tumanov <shurinskiy@users.noreply.github.com>
2020-10-11 14:47:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,142 @@
 'use strict';
-var path = require('path');
-var gutil = require('gulp-util');
-var through = require('through2');
-var SVGO = require('svgo');
-var svgParser = require('svg-parser');
-var jsontoxml = require('jsontoxml');
+const path = require('path');
+const SVGO = require('svgo');
+const Vinyl = require('vinyl');
+const through = require('through2');
+const jsontoxml = require('jsontoxml');
+const svgParser = require('svg-parser');
+const PluginError = require('plugin-error')
 
-var mapChild = function(child) {
-    var mappedChild = { name: child.name, attrs: child.attributes };
-    if (child.children && child.children.length > 0) {
-        mappedChild.children = child.children.map(mapChild);
-    }
-    return mappedChild;
+let mapChild = (child) => {
+	let mappedChild = {
+		name: child.tagName,
+		attrs: child.properties
+	};
+	if (child.children && child.children.length > 0) {
+		mappedChild.children = child.children.map(mapChild);
+	}
+	return mappedChild;
 };
 
-module.exports = function(output, opts) {
-    var filename;
-    var jsonFileName;
-    opts = opts || {
-        plugins: [ {removeViewBox: false, removeAttrs: { attrs: 'fill' } } ]
-    };
+let clone = (obg) => {
+	return JSON.parse(JSON.stringify(obg));
+}
 
-    if (typeof output === 'String') {
-        filename = output;
-    } else {
-        filename = output.svg;
-        jsonFileName = output.json;
-    }
+module.exports = (opt) => {
 
-    if (!filename) {
-        throw new gutil.PluginError('gulp-svg-symbol-sprite', 'filename is required');
-    }
+	let options = {
+		name: 'sprite.svg',
+		monochrome: false,
+		svgo: { plugins: [] }
+	};
+	
+	if (typeof opt === 'String') {
+		options.name = opt;
+	} else {
+		options = {...options, ...opt };
+		options.svgo.plugins.push({ removeViewBox: false });
+		if (options.monochrome) {
+			options.svgo.plugins.push({ removeAttrs: { attrs: '(fill|stroke)' }});
+		} 
+	}
 
-    var svgo = new SVGO(opts);
-    var symbols = [];
-    var latestFile;
+	let svgo = new SVGO(options.svgo);
+	let symbols = [];
+	let views = [];
+	let uses = [];
+	let shift = 0;
+	let latestFile;
 
-    return through.obj(
-        function(file, enc, cb) {
-            if (!file.isNull() && !file.stat.isDirectory()) {
-                latestFile = file;
+	return through.obj(
+		(file, enc, cb) => {
+			
+			if (!file.isNull() && !file.stat.isDirectory()) {
+				latestFile = file;
+				
+				let id = path.parse(file.path).name;
+				let json;
 
-                var id = path.parse(file.path).name;
-                var json;
-                svgo.optimize(file.contents).then(function(result) {
-                    try {
-                        json = svgParser.parse(result.data);
-                    } catch (err) {
-                        gutil.log(gutil.colors.red('gulp-svg-symbol-sprite'), 'Skipped file: ' + path.parse(file.path).base, 'could not parse file');
-                        return cb();
-                    }
+				svgo.optimize(file.contents).then((result) => {
+					try {
+						json = svgParser.parse(result.data);
+					} catch (err) {
+						return cb(new PluginError('gulp-svg-to-symbol', `Skipped file: ${path.parse(file.path).base}, could not parse file`));						
+					}
 
-                    var symbol = {
-                        name: 'symbol',
-                        attrs: {
-                            id: id,
-                            viewBox: json.attributes.viewbox || json.attributes.viewBox
-                        },
-                        children: json.children.map(mapChild)
-                    };
-                    symbols.push(symbol);
-                    cb();
-                });
-            }
-        },
-        function(cb) {
-            var data = '<svg height="0" style="position: absolute" width="0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' + jsontoxml(symbols) + '</svg>';
-            if (jsonFileName) {
-                this.push(
-                    new gutil.File({
-                        cwd: latestFile.cwd,
-                        base: latestFile.base,
-                        path: path.join(latestFile.base, jsonFileName),
-                        contents: Buffer.from(JSON.stringify(symbols))
-                    })
-                );
-            }
+					let viewbox = json.children[0].properties.viewbox || json.children[0].properties.viewBox;
+					let viewbox_arr = viewbox.split(' ');
+					let height = Number(viewbox_arr[3]) + 10; // высота текущего элемента с отступом
 
-            this.push(
-                new gutil.File({
-                    cwd: latestFile.cwd,
-                    base: latestFile.base,
-                    path: path.join(latestFile.base, filename),
-                    contents: Buffer.from(data)
-                })
-            );
-            cb();
-        }
-    );
+					viewbox_arr[1] = shift; // смещение по y (вниз)
+
+					symbols.push({
+						name: 'symbol',
+						attrs: {
+							id: id,
+							viewBox: viewbox
+						},
+						children: json.children[0].children.map(mapChild)
+					});
+
+					let view = {
+						name: 'view',
+						attrs: {
+							id: `${id}-view`,
+							viewBox: viewbox_arr.join(' ')
+						}
+					}
+
+					let use = {
+						name: 'use',
+						attrs: {
+							'xlink:href': `#${id}`,
+							x: viewbox_arr[0],
+							y: viewbox_arr[1],
+							width: viewbox_arr[2],
+							height: viewbox_arr[3]
+						}
+					}
+
+					if (options.monochrome) {
+						let ctr = 0;
+						for (const color in options.monochrome) {
+							let current_shift = shift + height*ctr;
+							
+							view.attrs.id = `${id}-view-${color}`;
+							use.attrs.style = `fill:${options.monochrome[color]}`;
+
+							view.attrs.viewBox = `${viewbox_arr[0]} ${current_shift} ${viewbox_arr[2]} ${viewbox_arr[3]}`;
+							use.attrs.y = current_shift;
+
+							uses.push(clone(use));
+							views.push(clone(view));
+
+							ctr++;
+						}
+						shift += height*ctr;
+					} else {
+						uses.push(use);
+						views.push(view);
+						shift += height;
+					}
+					
+					cb();
+				});
+			}
+		},
+		function (cb) {
+			let data = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">${jsontoxml(symbols)} ${jsontoxml(views)} ${jsontoxml(uses)}</svg>`;
+
+			this.push(
+				new Vinyl({
+					cwd: latestFile.cwd,
+					base: latestFile.base,
+					path: path.join(latestFile.base, options.name),
+					contents: Buffer.from(data)
+				})
+			);
+			cb();
+		}
+	);
 };
+
